7e9e652aec0b92a4f9ac41096a2d24279462a647
furokl <70150974+furokl@users.noreply.github.com>
2020-10-09 18:14:14 +0300

    + Комментарии + <cassert> для конструктора

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,4 +1,22 @@
-﻿#include <iostream>
+﻿// Выполнил ст. группы М3О-214Б-19.
+/*
+	Практическое задание №1 - класс изображения.
+
+0.	Необходимо разработать класс Image
+1.	Создать переменную типа Image и убедиться путем вставки отладочной печати, что конструктор
+	вызывается.
+2.	Реализовать методы get и set в предположение, что двумерное изображение помещается в 
+	одномерный массив data по строкам (первая строка, вторая строка и т.д.)
+3.	Реализовать метод show и убедиться, при создании переменной для изображения 10 на 10 на
+	экран выводится правильное изображение
+4.	Добавить в класс метод для рисования вертикальных линий. Убедиться, что они работают путем
+	выдачи изображения на экран при помощи метода show.
+5.	Реализовать метод для сохранения изображения в файл в формате pnm
+	(https://ru.wikipedia.org/wiki/Portable_anymap)
+*/
+
+#include <iostream>
+#include <cassert>
 
 using std::cout;
 using std::endl;
@@ -14,8 +32,13 @@ public:
 	Image(int tmx = 200, int tmy = 200) 
 		: mx(tmx), my(tmy)
 	{
+		// Подразумевается, что картинка максимум 200x200, поскольку тяжело развернуть в консоли
+
 		for (int i = 0; i < tmx*tmy; i++)
 			data[i] = 0;
+
+		assert(tmx >= 1 && tmx <= 200);
+		assert(tmy >= 1 && tmy <= 200);
 	}
 
 	
@@ -31,12 +54,11 @@ public:
 				cout << '\n';
 		}
 	}
-
+	// set x*y
 	void set(int x, int y, int color) {
 		cout << "\t\t*Setter used*\n\tx: " << x
 			<< "\n\ty: " << y
 			<< "\n\tcolor: " << color << endl;
-
 		if (x < 1 || x > 200)	return;
 		if (y < 1 || y > 200)	return;
 
@@ -45,7 +67,7 @@ public:
 		for (int i = 0; i < x*y; i++)
 			data[i] = color;
 	}
-
+	// set y
 	void set(int x, int color) {
 		cout << "\t\t*Setter used*\n\tx: " << x
 			<< "\n\tcolor: " << color << endl;
@@ -62,11 +84,12 @@ public:
 
 int main() {
 	cout << "\n\n\t\tProgramm started...\n\n";
-	Image a(3, 3);
+	Image a(20, 5);
 	a.show();
-	a.set(5, 5, 1);
+	a.set(10, 10, 1);
 	a.show();
-	a.set(2, 0);
+	a.set(5, 0);
+	a.set(6, 0);
 	a.show();
 
 	return 0;
